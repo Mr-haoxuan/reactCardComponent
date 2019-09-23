aab1068642c83b4d99c5ccb1c0b2d35fdc48c4f9
zhangchao <424306038@qq.com>
2019-09-23 15:11:58 +0800

    修改link属性，重写react组件

--- src/Card.js
+++ src/Card.js
@@ -1,47 +1,57 @@
 "use strict"
 
 import React from "react";
-import "./bootstrap.min.css"
 
-export default class Card extends React.Component{
-    render(){
-        const content = this.props.content;
-        let bodys = [];
-        let listGroup = [];
-        console.log(content.cardBodys)
-        content.cardBodys.forEach((element,index) => {
-            let aGroup = [];
-            element.links.forEach((e,i) =>{
-                let a = <a className="btn btn-warning " key={i} href={e.linkurl}>{e.linkname}</a>
-                aGroup.push(a)
-                return aGroup
+export default class Card extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    showBodys() {
+        const { content: { cardBodys = [] } } = this.props;
+
+        return cardBodys.map((cardItem, index) => {
+            let showLinks = cardItem.links.map((e, i) => {
+                return <a className="btn btn-warning" key={i} href={e.linkurl}>{e.linkname}</a>
             })
-            console.log(element);
-            let body = <div className="card-body text-white bg-info" key={index}>
-                <h4 className="card-title">{element.title}</h4>
-                <h6 className="card-subtitle">{element.subtitle}</h6>
-                <p className="card-text">{element.text}</p>
-                <p>
-                    {aGroup}
-                </p> 
-            </div>;
-            bodys[index] = body;
-            return bodys
-        });
-        content.listGroup.forEach((element,index) => {
-            let list = <li className="list-group-item" key={index}>
-                {element}
-            </li> 
-            listGroup.push(list)
-            return <ul>{listGroup}</ul>
-        });
-        console.log(bodys)
-        return (<div className="card text-center" style={{width: 18+"rem"}}>
-            <div className="card-header">{content.header}</div>
-            <img className="card-img-top" src={content.img}/>
-            {bodys}
-            <ul className="list-group list-group-flush">{listGroup}</ul>
-            <p className="card-footer">{content.footer}</p>
-        </div>)
+
+            return (
+                <div className="card-body text-white bg-info" key={index}>
+                    <h4 className="card-title">{cardItem.title}</h4>
+                    <h6 className="card-subtitle">{cardItem.subtitle}</h6>
+                    <p className="card-text">{cardItem.text}</p>
+                    <p>
+                        {showLinks}
+                    </p>
+                </div>
+            )
+        })
+    }
+
+    showListGroup = () => {
+        const { content: { listGroup = [] } } = this.props;
+
+        return listGroup.map((listItem, index) => {
+            return (
+                <li className="list-group-item" key={index}>
+                    {listItem}
+                </li>
+            )
+        })
+    }
+
+    render() {
+        const { content } = this.props;
+
+        return (
+            <div className="card text-center" style={{ width: 18 + "rem" }}>
+                <div className="card-header">{content.header}</div>
+                <img className="card-img-top" src={content.img} />
+                {this.showBodys()}
+                <ul className="list-group list-group-flush">{this.showListGroup()}</ul>
+                <p className="card-footer">{content.footer}</p>
+            </div>
+        )
     }
 }
